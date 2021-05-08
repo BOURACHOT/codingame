e9470e603408fbeaab9c6edf18cc2233b7f6098d
joel bourachot <joel.bourachot@orange.fr>
2021-05-08 10:28:16 +0200

    test 01 ok

---
+++ Puzzle classique-Facile/Van eck' seguence/001.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+fscanf(STDIN, "%d", $A1);
+fscanf(STDIN, "%d", $N);
+
+ //initialisation variables
+	 $i=1;
+     $terme[$i]=$A1;
+	$cible=$N; 			
+                
+while ($i<$cible)
+
+	{
+			for ($j = $i; $j <1; $j--)
+
+					 {
+
+
+
+						 if ( $terme[$j] = $terme[$j-1])
+						
+							 $terme[$j+1]=1;
+
+					 }
+                     $i=$i+1;
+
+				$terme[$i]=0;
+
+}
+	 
+
+
+$answer=$terme[$cible];
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+
+
+
+echo("$answer\n");
+?>
